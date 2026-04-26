abd347578f97913e90bef5d8ca38d251fc4fa91e
Anna Butenko <anyab24@gmail.com>
2026-04-26 12:53:13 +0200

    fix: Trace retain on failure

--- playwright.config.ts
+++ playwright.config.ts
@@ -13,7 +13,7 @@ export default defineConfig({
     : [["html", { open: "never" }]],
   use: {
     baseURL: env.BASE_URL,
-    trace: 'on-first-retry',
+    trace: 'retain-on-failure',
   },
 
   projects: [
